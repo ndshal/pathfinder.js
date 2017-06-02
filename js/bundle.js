7acc30f1d0689b5e7c2a7bc531a867bf75092e1c
Nikita Shalimov <nshalimov@berkeley.edu>
2017-06-02 12:11:53 -0700

    Rebundle

--- js/bundle.js
+++ js/bundle.js
@@ -252,9 +252,9 @@
 	  }, {
 	    key: 'init',
 	    value: function init() {
-	      // this.setupSimple();
-	      this.setStart(10 * this.dx + ',' + 10 * this.dy);
-	      this.setGoal(26 * this.dx + ',' + 10 * this.dy);
+	      this.setupSimple();
+	      // this.setStart(`${10*this.dx},${10*this.dy}`);
+	      // this.setGoal(`${26*this.dx},${10*this.dy}`);
 	      createjs.Ticker.addEventListener('tick', this.stage);
 	    }
 	  }, {
