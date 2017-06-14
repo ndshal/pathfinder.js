60cd3c7f77db81345fb90bf746d63e4786cc2651
Nikita Shalimov <ndshalimov@gmail.com>
2017-06-14 10:23:36 -0700

    Add correct path to new maze preset

--- js/board.js
+++ js/board.js
@@ -48,6 +48,11 @@ class Board {
   }
 
   handleClick(e) {
+    console.log([
+      Math.floor(e.stageX/this.dx),
+      Math.floor(e.stageY/this.dy),
+    ].toString());
+
     if (this.allowPaint) {
       const node = this.grid[this._getCoordsFromEvent(e)];
       node.toggleIsObstacle();
